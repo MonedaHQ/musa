a4ca96ed529b275a877d602ff2c7b95b71ffc245
okolipraiz-moneda <praise.o@monedainvest.com>
2026-04-15 14:56:51 +0100

    Add background image SVG for enhanced visual design

--- src/pages/homepage/Herosection.jsx
+++ src/pages/homepage/Herosection.jsx
@@ -7,8 +7,8 @@ import styles from './styles/herosection.module.css';
 function Herosection() {
   return (
     <div className={styles.heroSection}>
-      <div className={styles.ellipseTop} />
-      <div className={styles.ellipseBottom} />
+      {/* <div className={styles.ellipseTop} />
+      <div className={styles.ellipseBottom} /> */}
       <div className={styles.container}>
         <HeroContent />
         <motion.div
@@ -16,16 +16,18 @@ function Herosection() {
           initial={{ y: 100, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           viewport={{ once: true }}
-          transition={{ duration: 0.8, ease: 'easeOut' }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
         >
-          <Image
-            src="/assets/screens/dashboard.png"
-            alt="Musa Dashboard Interface"
-            width={2880}
-            height={1800}
-            priority
-            draggable={false}
-          />
+          <div>
+            <Image
+              src="/assets/screens/dashboard.svg"
+              alt="Musa Dashboard Interface"
+              width={2880}
+              height={1800}
+              priority
+              draggable={false}
+            />
+          </div>
         </motion.div>
       </div>
     </div>
@@ -36,12 +38,12 @@ function HeroContent() {
   return (
     <div className={styles.heroContent}>
       <WordAnimator
-        text="Imagine Private Credit, But Without Fear"
+        text="Imagine Private Credit, <br /> But Without Fear"
         highlightWords={{ Fear: 'var(--color-orange)' }}
         as="h1"
       />
       <h4>
-        Musa is a unified credit management and payment infrastructure built to
+        Musa is a unified credit management and payment infrastructure <br /> built to
         facilitate trade in African markets by mitigating risk.
       </h4>
       <div className={styles.ctaButtons}>
